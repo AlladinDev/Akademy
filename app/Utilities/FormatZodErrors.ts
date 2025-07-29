e1045448c00aea37a9ed77a8e83837302289bf9d
saqib mushtaq <irfanrather4627@gmail.com>
2025-07-29 15:18:18 +0530

    Fixed Utility fn FormatZodErrors.ts removed unused imports and change let key from let to const

--- app/Utilities/FormatZodErrors.ts
+++ app/Utilities/FormatZodErrors.ts
@@ -1,9 +1,9 @@
-import { ZodIssue } from "zod/v3";
+
 import { $ZodIssue } from "zod/v4/core";
 
 export const FormatZodErrors = (errors: $ZodIssue[]) => {
     const formattedErrors = {}
-    for (let key in errors) {
+    for (const key in errors) {
         formattedErrors[errors[key].path[0]] = errors[key].message
     }
     return formattedErrors
